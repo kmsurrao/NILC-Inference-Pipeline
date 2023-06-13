fix cov matrix calculation

nilc_pipeline/acmb_atsz_nilc.py
@@ -28,7 +28,7 @@ def get_PScov_sim(inp, Clpq_unscaled):
     Clpq_tmp_means = np.mean(Clpq_tmp, axis=2)
     # cov[b1,b2,i,j] is sum over b1,b2,i,j,sim of (Clpq_tmp[b1,i,sim]-Clpq_tmp_means[b1,i])*(Clpq_tmp[b2,j,sim]-Clpq_tmp_means[b2,j])
     cov = np.einsum('bis,cjs->bcij', Clpq_tmp, Clpq_tmp) - np.einsum('bis,cj->bcij', Clpq_tmp, Clpq_tmp_means) \
-        - np.einsum('bi,cjs->bcij', Clpq_tmp_means, Clpq_tmp) + np.einsum('bi,cj->bcij', Clpq_tmp_means, Clpq_tmp_means)
+        - np.einsum('bi,cjs->bcij', Clpq_tmp_means, Clpq_tmp) + inp.Nsims*np.einsum('bi,cj->bcij', Clpq_tmp_means, Clpq_tmp_means)
     cov /= (inp.Nsims-1)
     return cov
 

template_fitting_pipeline/acmb_atsz.py
@@ -60,7 +60,7 @@ def get_PScov_sim(inp, Clij):
     Clij_tmp_means = np.mean(Clij_tmp, axis=2)
     # cov[b1,b2,i,j] is sum over b1,b2,i,j,sim of (Clij_tmp[b1,i,sim]-Clij_tmp_means[b1,i])*(Clij_tmp[b2,j,sim]-Clij_tmp_means[b2,j])
     cov = np.einsum('bis,cjs->bcij', Clij_tmp, Clij_tmp) - np.einsum('bis,cj->bcij', Clij_tmp, Clij_tmp_means) \
-        - np.einsum('bi,cjs->bcij', Clij_tmp_means, Clij_tmp) + np.einsum('bi,cj->bcij', Clij_tmp_means, Clij_tmp_means)
+        - np.einsum('bi,cjs->bcij', Clij_tmp_means, Clij_tmp) + inp.Nsims*np.einsum('bi,cj->bcij', Clij_tmp_means, Clij_tmp_means)
     cov /= (inp.Nsims-1)
     return cov
 
@@ -143,7 +143,7 @@ def acmb_atsz(inp, sim, Clij00_all_sims, Clij01_all_sims, Clij10_all_sims, Clij1
     -------
     best fit Acmb, Atsz, Anoise1, Anoise2 (floats)
     '''
-    bounds = ((0.001, None), (0.001, None), (0.001, None), (0.001, None))
+    bounds = ((0.001, 1000), (0.001, 1000), (0.001, 1000), (0.001, 1000))
     all_res = []
     for start in [0.5, 1.0, 1.5]:
         start_array = [start, start, start, start] #acmb_start, atsz_start, anoise1_start, anoise2_start

template_fitting_pipeline/main.py
@@ -91,14 +91,16 @@ def main():
     #set up output directory
     setup_output_dir(inp, my_env)
 
-    pool = mp.Pool(inp.num_parallel)
-    Clij = pool.starmap(get_data_vectors, [(sim, inp) for sim in range(inp.Nsims)])
-    pool.close()
-    Clij = np.asarray(Clij, dtype=np.float32) #shape (Nsims, Nfreqs=2, Nfreqs=2, Ncomps=3, Nbins)
-    if inp.save_files:
-        pickle.dump(Clij, open(f'{inp.output_dir}/data_vecs/Clij.p', 'wb'), protocol=4)
-        if inp.verbose:
-            print(f'saved {inp.output_dir}/data_vecs/Clij.p')
+    # pool = mp.Pool(inp.num_parallel)
+    # Clij = pool.starmap(get_data_vectors, [(sim, inp) for sim in range(inp.Nsims)])
+    # pool.close()
+    # Clij = np.asarray(Clij, dtype=np.float32) #shape (Nsims, Nfreqs=2, Nfreqs=2, Ncomps=3, Nbins)
+    # if inp.save_files:
+    #     pickle.dump(Clij, open(f'{inp.output_dir}/data_vecs/Clij.p', 'wb'), protocol=4)
+    #     if inp.verbose:
+    #         print(f'saved {inp.output_dir}/data_vecs/Clij.p')
+
+    Clij = pickle.load(open(f'{inp.output_dir}/data_vecs/Clij.p', 'rb')) #remove this line and uncomment above
     
     acmb_array, atsz_array, anoise1_array, anoise2_array = get_all_acmb_atsz(inp, Clij)
     